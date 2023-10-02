3d6f788467e2bbccb6114bef1abf91b55278a790
Takendrar <taakendrarawat2055@gmail.com>
2023-10-02 08:33:02 +0545

    few css chang

--- src/page/Home.js
+++ src/page/Home.js
@@ -5,10 +5,19 @@ function Home() {
   return (
     <>
       <div
-        className="w-full h-[600px] box-border bg-cover"
-        style={{ backgroundImage: `url(${CafeImg})` }}>
-                <p className="bg-black text-white inline-block ">Open from 6am to 8pm </p>
-        </div>
+        className="w-full h-[600px] box-border bg-cover relative"
+        style={{ backgroundImage: `url(${CafeImg})` }}
+      >
+        <p className="text-white flex justify-center items-center w-screen h-full text-9xl bg-opacity-40 bg-black">
+          The <br /> Cafe{" "}
+        </p>
+        <p className="bg-black text-white inline-block absolute bottom-3 left-8 px-2 py-1">
+          Open from 6am to 8pm{" "}
+        </p>
+        <p className="text-white absolute bottom-3 right-2">
+          Birendranagar, Surkhet
+        </p>
+      </div>
     </>
   );
 }
